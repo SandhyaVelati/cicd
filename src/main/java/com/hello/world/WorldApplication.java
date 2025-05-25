e1b2aa9d9a811ef6eda9d91c082058acf60cb9b3
sandhya <svuksept2020@gmail.com>
2025-05-25 23:03:41 +0100

    chore: ci checks

--- src/main/java/com/hello/world/WorldApplication.java
+++ src/main/java/com/hello/world/WorldApplication.java
@@ -8,6 +8,7 @@ public class WorldApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WorldApplication.class, args);
+		System.out.println("just checking cicd");
 	}
 
 }
